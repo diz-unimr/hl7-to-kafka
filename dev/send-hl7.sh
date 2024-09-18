7a3ce4baa775bd55fef782a2104daecbccf4950b
Sebastian Stöcker <sebastian.stoecker@uni-marburg.de>
2024-09-18 14:02:09 +0200

    feat: add Kafka key expression selector

--- dev/send-hl7.sh
+++ dev/send-hl7.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo -n -e "\x0b$(cat hl7v2-mllp-sample.txt)\x1c\x0d" | nc localhost 2575
+echo -n -e "\x0b$(cat hl7v2-oru-r01-sample.hl7)\x1c\x0d" | nc localhost 2575
--- src/main/java/de/unimarburg/diz/hl7tokafka/Hl7Router.java
+++ src/main/java/de/unimarburg/diz/hl7tokafka/Hl7Router.java
@@ -1,8 +1,12 @@
 package de.unimarburg.diz.hl7tokafka;
 
+import static org.apache.camel.component.hl7.HL7.hl7terser;
+
+import io.micrometer.common.util.StringUtils;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import org.apache.camel.Expression;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.endpoint.EndpointRouteBuilder;
 import org.apache.camel.component.hl7.HL7Constants;
@@ -19,37 +23,51 @@ public class Hl7Router extends EndpointRouteBuilder {
     private final String hl7Port;
     private final String encoding;
     private final String kafkaTopic;
+    private final Expression kafkaKeyExpression;
 
 
     public Hl7Router(@NotNull @Value("${endpoint.hl7.port}") String hl7Port,
         @NotNull @Value("${endpoint.hl7.encoding}") String encoding,
-        @NotNull @Value("${endpoint.kafka.topic}") String kafkaTopic) {
+        @NotNull @Value("${endpoint.kafka.topic}") String kafkaTopic,
+        @Value("${endpoint.kafka.key-expression:#{null}}")
+        String kafkaKeyExpression) {
         this.hl7Port = hl7Port;
         this.encoding = encoding;
         this.kafkaTopic = kafkaTopic;
+        this.kafkaKeyExpression =
+            StringUtils.isBlank(kafkaKeyExpression) ? header(
+                HL7Constants.HL7_MESSAGE_CONTROL)
+                : hl7terser(kafkaKeyExpression);
     }
 
     @SuppressWarnings("checkstyle:LineLength")
     @Override
     public void configure() {
 
-        from(mllp("0.0.0.0:" + hl7Port).charsetName(encoding))
-            .routeId("hl7Listener").unmarshal().hl7().log(LoggingLevel.DEBUG,
-                "Message received: ${header.CamelHL7MessageControl}").process(
-                ex -> ex.getIn().setHeader(KafkaConstants.OVERRIDE_TIMESTAMP,
-                    convertTimestamp(ex.getIn()
+        from(mllp("0.0.0.0:" + hl7Port).charsetName(encoding)).routeId(
+                "hl7Listener")
+            .unmarshal()
+            .hl7(false)
+            .log(LoggingLevel.DEBUG,
+                "Message received: ${header.CamelHL7MessageControl}")
+            .process(ex -> ex.getIn()
+                .setHeader(KafkaConstants.OVERRIDE_TIMESTAMP, convertTimestamp(
+                    ex.getIn()
                         .getHeader(HL7Constants.HL7_TIMESTAMP, String.class))))
 
             .log(LoggingLevel.DEBUG,
                 "Timestamp converted with tz: " + ZoneId.systemDefault())
-            .setHeader(KafkaConstants.KEY,
-                header(HL7Constants.HL7_MESSAGE_CONTROL)).to(kafka(kafkaTopic))
+            .setHeader(KafkaConstants.KEY, kafkaKeyExpression)
+            .to(kafka(kafkaTopic))
             .log(LoggingLevel.DEBUG,
-                "Message send to Kafka topic: " + kafkaTopic).end();
+                "Message send to Kafka topic: " + kafkaTopic)
+            .end();
     }
 
     private long convertTimestamp(String dateString) {
         return LocalDateTime.parse(dateString, DATE_FORMATTER)
-            .atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+            .atZone(ZoneId.systemDefault())
+            .toInstant()
+            .toEpochMilli();
     }
 }
